7c938cc1b81d6e38a5db13d205fd1ee324c9b4a1
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2024-09-12 15:10:12 +0900

    ✨ Feat : 이미 가입한 회원일 경우 랜딩 페이지에서 바로 로그인 페이지로 갈 수 있도록 링크 추가 #208

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
+import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
@@ -29,6 +30,11 @@ export default function HomePage() {
           간편하게 일기를 기록하고 다양한 사람들과 공유해보세요
         </p>
       </div>
+      <Link
+        href="/login"
+        className="fixed bottom-20 w-5/6 left-1/2 -translate-x-1/2 text-secondary text-center">
+        이미 계정이 있으신가요?
+      </Link>
       <Button
         type="button"
         className="fixed bottom-6 w-5/6 max-w-96 left-1/2 -translate-x-1/2 bg-mainColor text-black font-bold "
