e41e39c839e6133262439851bc9710c10888afb7
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2024-09-19 10:47:16 +0900

    💄 Style : 회원가입 페이지의 프로필 이미지 파일 선택 버튼 스타일 수정 #206

--- app/(onboarding)/(auth)/signup/page.tsx
+++ app/(onboarding)/(auth)/signup/page.tsx
@@ -204,7 +204,7 @@ export default function SignupPage() {
                           form.getValues("attach")?.[0]
                             ? ""
                             : "text-secondary text-center"
-                        } h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background truncate`}
+                        } h-10 w-full rounded-md bg-backgroundLighter px-3 py-2 text-sm ring-offset-background truncate`}
                         htmlFor="attach">
                         {form.getValues("attach")?.[0]
                           ? fileInputValue
