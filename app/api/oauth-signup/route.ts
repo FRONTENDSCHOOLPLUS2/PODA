661496a745e75acfecf831e8d1092991e20ba5ae
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2024-09-19 23:36:58 +0900

    🐛 Modify : 로그인 시 리프레시 토큰 재발급이 정상적으로 되지 않는 문제 수정 #214

--- app/api/oauth-signup/route.ts
+++ app/api/oauth-signup/route.ts
@@ -24,7 +24,7 @@ export async function GET(request: NextRequest) {
         "client-id": "09-triots",
       },
       body: JSON.stringify({
-        refreshToken: resLogin?.item?.token?.refreshToken,
+        refreshToken: session?.refreshToken,
         extra: {
           ...resLogin.item.extra,
           isOnboarding: true,
