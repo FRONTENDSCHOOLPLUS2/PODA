d4588c25bb7df1d04f3be92bbd08266846f8561a
seok-hyung <korea970223@naver.com>
2024-10-04 17:06:12 +0900

    :sparkles:Feat : 카메라 촬영 및 갤러이에서 불러온 이미지 추가 기능 구현

--- app/(main)/mydiary/new/write-diary/diary-step.tsx
+++ app/(main)/mydiary/new/write-diary/diary-step.tsx
@@ -2,7 +2,7 @@
 import { NavigationHeader } from "@/components/navigation-header"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import Image from "next/image"
-import React, { ChangeEvent, useState } from "react"
+import React, { ChangeEvent, useRef, useState } from "react"
 import { Tabs } from "../../components/tabs"
 import { TabDatas } from "../../components/tab-datas"
 import { DataPreview } from "../../components/data-preview"
@@ -13,6 +13,12 @@ import { TagData, datas, tabData } from "../../data"
 import { Button } from "@/components/ui/button"
 import { useAddPost } from "@/hooks/mutation/post"
 import { FullScreen } from "@/components/spinner"
+import { ImCamera } from "react-icons/im"
+import { TbFolderFilled } from "react-icons/tb"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { cn } from "@/lib/utils"
+import { postFormRequest } from "@/lib/protocol"
 
 export const DiaryStep = () => {
   const {
@@ -22,10 +28,8 @@ export const DiaryStep = () => {
     selectedTags,
     activeTags,
     seter,
-    resetValues,
   } = useDiaryValues()
   const { push } = useRouter()
-  // const [activeTags, setActiveTags] = useState<{ [key: string]: boolean }>({})
   const [activeTabIndex, setActiveTabIndex] = useState<number>(0)
   const currentTabDatas = Object.values(
     datas[tabData[activeTabIndex] as keyof TagData]
@@ -81,26 +85,94 @@ export const DiaryStep = () => {
     seter(e.target.value, "noteContentVal")
   }
 
-  const handleSave = () => {
-    const requestBody = {
-      type: "mydiary",
-      extra: {
-        title: noteTitleVal,
-        content: noteContentVal,
-        mood: moodVal,
-        tag: selectedTags ? [...selectedTags] : [],
-      },
-    }
+  const handleSave = async () => {
+    // 이미지를 업로드
+    if (fileInputValue.length > 0) {
+      const body = new FormData()
+      fileInputValue.forEach((value) => {
+        body.append("attach", value)
+      })
 
-    try {
-      mutate(requestBody)
-    } catch (error) {
-      console.log(error)
-    } finally {
-      push("/mydiary")
-      resetValues()
+      const fileRes = await postFormRequest(`${SERVER}/files`, body)
+
+      if (!fileRes.ok) {
+        throw new Error("파일 업로드 실패입니다.")
+      }
+
+      // 이미지 경로를 설정
+      const newImagePathList = fileRes.item.map((value: any) => value.path)
+
+      // requestBody를 업로드 후 구성
+      const requestBody = {
+        type: "mydiary",
+        extra: {
+          title: noteTitleVal,
+          content: noteContentVal,
+          mood: moodVal,
+          tag: selectedTags ? [...selectedTags] : [],
+          attach: newImagePathList, // 즉시 업데이트된 경로 사용
+        },
+      }
+
+      try {
+        mutate(requestBody)
+        push("/mydiary")
+      } catch (error) {
+        console.log(error)
+      } finally {
+        // resetValues();
+      }
     }
   }
+
+  const swipeRef = useRef<HTMLDivElement>(null)
+  const startXRef = useRef<number>(0)
+  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    startXRef.current = e.touches[0].clientX
+  }
+  const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
+    const currentX = e.touches[0].clientX
+    const diffX = startXRef.current - currentX
+
+    if (Math.abs(diffX) > swipeRef.current?.clientWidth! * 0.5) {
+      if (diffX > 0) {
+        handleChevronRightClick()
+      } else {
+        handleChevronLeftClick()
+      }
+      startXRef.current = 0
+    }
+  }
+
+  const SERVER = process.env.NEXT_PUBLIC_API_URL
+
+  const [fileInputValue, setFileInputValue] = useState<File[]>([])
+
+  const handleCameraClick = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+    if (file) {
+      setFileInputValue((prevFiles) => [...prevFiles, file])
+    }
+  }
+
+  const handleGalleryClick = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = Array.from(e.target.files || [])
+    setFileInputValue((prevFiles) => [...prevFiles, ...files])
+  }
+  const handleRemoveImage = (name: string) => {
+    setFileInputValue((prevFiles) =>
+      prevFiles.filter((file) => file.name !== name)
+    )
+  }
+
+  const getImageWidth = () => {
+    const count = fileInputValue.length
+    if (count === 1) return "w-full h-64"
+    if (count === 2 || count === 4) return "w-[calc(50%-4px)] h-36"
+    if (count === 3 || count >= 5) return "w-[calc(33%-5px)] h-28"
+    return ""
+  }
+
   return (
     <>
       <NavigationHeader isBack isNew isMood isSave />
@@ -132,7 +204,11 @@ export const DiaryStep = () => {
         </div>
 
         {/* 현재 탭 데이터들 */}
-        <div className="flex flex-wrap bg-backgroundLighter w-full rounded-[6px] mb-4 p-6 gap-2">
+        <div
+          ref={swipeRef}
+          onTouchStart={handleTouchStart}
+          onTouchMove={handleTouchMove}
+          className="flex flex-wrap bg-backgroundLighter w-full rounded-[6px] mb-4 p-6 gap-2">
           <TabDatas
             currentTabDatas={currentTabDatas}
             handleTagClick={handleTagClick}
@@ -160,6 +236,67 @@ export const DiaryStep = () => {
           />
         </div>
 
+        {/* 사진 */}
+        <div className="mb-6">
+          <div className="flex gap-2 mb-2">
+            <ImCamera className="text-mainColor w-7 h-7" />
+            <h2>사진</h2>
+          </div>
+          <div className="flex justify-between gap-2">
+            <Label
+              htmlFor="camera"
+              className="flex flex-1 cursor-pointer items-center justify-center bg-[#555555] h-10 px-4 py-2 rounded-md ">
+              <ImCamera className="text-mainColor w-5 h-5 mr-2" />
+              <Input
+                id="camera"
+                accept="image/*"
+                type="file"
+                capture="camera" // 카메라로 직접 촬영
+                className="bg-[#555555] text-white hidden" // 숨김 처리
+                onChange={handleCameraClick}
+              />
+              사진 촬영
+            </Label>
+
+            <Label
+              htmlFor="attach"
+              className="flex-1 flex bg-[#555555] h-10 px-4 py-2 rounded-md items-center justify-center">
+              <TbFolderFilled className="text-mainColor w-6 h-6 mr-2" />
+              <Input
+                id="attach"
+                accept="image/*"
+                type="file"
+                multiple // 여러 파일 선택 가능
+                className="bg-[#555555] hidden" // 숨김처리
+                onChange={handleGalleryClick}
+              />
+              갤러리에서
+            </Label>
+          </div>
+        </div>
+
+        {/* 미리보기 이미지 */}
+        <div className="flex flex-wrap gap-2 mb-4">
+          {fileInputValue.map((file) => (
+            <div
+              key={file.name}
+              className={cn("relative overflow-hidden", getImageWidth())}>
+              <Image
+                src={URL.createObjectURL(file)}
+                alt={`Preview ${file.name}`}
+                width={200}
+                height={200}
+                className="rounded w-full h-full object-cover object-center"
+              />
+              <button
+                onClick={() => handleRemoveImage(file.name)}
+                className="absolute top-2 right-2 bg-red-500 rounded-full w-6 h-6 flex items-center justify-center text-red-800"
+                aria-label="Remove image">
+                &times;
+              </button>
+            </div>
+          ))}
+        </div>
         <Button
           variant="ghost"
           className="w-full bg-mainColor text-black font-extrabold mb-8"
